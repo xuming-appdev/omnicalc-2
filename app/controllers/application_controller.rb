b26362c60089815cb885badeb9d2c6d1b6958a42
xumingmonica101 <ming.xu@chicagobooth.edu>
2022-04-28 04:58:47 +0000

    Part 2: Omnicalc 2 Final

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,64 @@
 class ApplicationController < ActionController::Base
+    def addition_form
+      render("add_templates/muggles.html.erb")
+    end
+    
+    def addition_results
+      # Parameters: {"first_num"=>"42", "second_num"=>"10"}
+  
+      @first = params.fetch("first_num").to_f
+      @second = params.fetch("second_num").to_f
+  
+      @result = @first + @second
+  
+      render("add_templates/results.html.erb")
+    end
+
+
+    def subtract_form
+      render("subtract_templates/muggles.html.erb")
+    end
+    
+    def subtract_results
+      # Parameters: {"first_num"=>"42", "second_num"=>"10"}
+  
+      @first = params.fetch("first_num").to_f
+      @second = params.fetch("second_num").to_f
+  
+      @result = @second-@first
+  
+      render("subtract_templates/results.html.erb")
+    end
+
+    def multiply_form
+      render("multiply_templates/muggles.html.erb")
+    end
+    
+    def multiply_results
+      # Parameters: {"first_num"=>"42", "second_num"=>"10"}
+  
+      @first = params.fetch("first_num").to_f
+      @second = params.fetch("second_num").to_f
+  
+      @result = @second*@first
+  
+      render("multiply_templates/results.html.erb")
+    end
+
+
+    def divide_form
+      render("divide_templates/muggles.html.erb")
+    end
+    
+    def divide_results
+      # Parameters: {"first_num"=>"42", "second_num"=>"10"}
+  
+      @first = params.fetch("first_num").to_f
+      @second = params.fetch("second_num").to_f
+  
+      @result = @first/@second
+  
+      render("divide_templates/results.html.erb")
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,22 @@
 Rails.application.routes.draw do
 
+  get("/wizard_add", { :controller => "application", :action => "addition_results" })
+  
+  get("/add", { :controller => "application", :action => "addition_form" })
+
+  get("/wizard_subtract", { :controller => "application", :action => "subtract_results" })
+  
+  get("/subtract", { :controller => "application", :action => "subtract_form" })
+
+
+  get("/wizard_multiply", { :controller => "application", :action => "multiply_results" })
+  
+  get("/multiply", { :controller => "application", :action => "multiply_form" })
+
+  get("/wizard_divide", { :controller => "application", :action => "divide_results" })
+  
+  get("/divide", { :controller => "application", :action => "divide_form" })
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
